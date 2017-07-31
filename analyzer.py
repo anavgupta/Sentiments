63dcd10ab1cdd7e6071dd2bb8edf7a21ae402a26
Anav Gupta <anavgupta13@gmail.com>
2017-07-31 22:42:19 +0530

    Initial Commit

---
+++ analyzer.py
@@ -0,0 +1,16 @@
+import nltk
+
+
+class Analyzer():
+    """Implements sentiment analysis."""
+
+    def __init__(self, positives, negatives):
+        """Initialize Analyzer."""
+
+        # TODO
+
+    def analyze(self, text):
+        """Analyze text for sentiment, returning its score."""
+
+        # TODO
+        return 0
